eb53337beb52d5a05355932a4b8485e856cc261b
Jhon Salguero <svjchrysler@gmail.com>
2017-01-14 22:31:27 -0400

    config heroku deploy

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-
 	"os"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/kataras/iris"
@@ -19,14 +17,12 @@ const (
 	USER = "kaytqbatuadino"
 	//DB database
 	DB = "d1471skb605131"
-	//SSL database
-	SSL = "disable"
 	//PASSWORD database
 	PASSWORD = "2eb0d490c52f84554882caf3b8acd2dc4613cd76702410bf23e47a5a5c962ead"
 )
-
+//conection a database
 func conection() *gorm.DB {
-	conexion := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s", HOST, USER, DB, SSL, PASSWORD)
+	conexion := fmt.Sprintf("host=%s user=%s dbname=%s password=%s", HOST, USER, DB, PASSWORD)
 	db, e := gorm.Open("postgres", conexion)
 	if e != nil {
 		log.Fatal(e)
